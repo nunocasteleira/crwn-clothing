304975c4f69ad29d2f3ab12cece6acc7944f8369
Nuno Casteleira [1201775] <1201775@isep.ipp.pt>
2021-03-11 19:27:06 +0000

    add redux and implement userReducer and userActions to header and app components

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+// similar to this.state inside the constructor
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
